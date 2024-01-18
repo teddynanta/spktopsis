d2d8a1329c500d9e4cd61f6cc153315b3b26709a
teddynanta <teddy.nanta99@gmail.com>
2024-01-18 21:52:45 +0700

    ubah struktur db

--- spk_topsis.sql
+++ spk_topsis.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2024 at 09:27 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Jan 18, 2024 at 03:52 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -35,7 +36,7 @@ CREATE TABLE `data` (
   `data4` text NOT NULL,
   `data5` text NOT NULL,
   `data6` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `data`
@@ -53,7 +54,7 @@ INSERT INTO `data` (`id`, `data1`, `data2`, `data3`, `data4`, `data5`, `data6`) 
 CREATE TABLE `tab_alternatif` (
   `id_alternatif` varchar(10) NOT NULL,
   `nama_alternatif` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tab_alternatif`
@@ -75,18 +76,18 @@ CREATE TABLE `tab_kriteria` (
   `nama_kriteria` varchar(50) NOT NULL,
   `bobot` float NOT NULL,
   `status` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tab_kriteria`
 --
 
 INSERT INTO `tab_kriteria` (`id_kriteria`, `nama_kriteria`, `bobot`, `status`) VALUES
-('1', 'Pekerjaan', 2, 'Benefit'),
-('2', 'Hutang', 3, 'Cost'),
-('3', 'Penghasilan', 5, 'Benefit'),
-('4', 'Pengeluaran', 4, 'Cost'),
-('5', 'Simpanan', 1, 'Benefit');
+('10', 'Kondisi', 1, 'Cost'),
+('6', 'Karakter', 3, 'Benefit'),
+('7', 'Kapasitas', 4, 'Benefit'),
+('8', 'Modal', 5, 'Benefit'),
+('9', 'Jaminan', 2, 'Benefit');
 
 -- --------------------------------------------------------
 
@@ -97,28 +98,31 @@ INSERT INTO `tab_kriteria` (`id_kriteria`, `nama_kriteria`, `bobot`, `status`) V
 CREATE TABLE `tab_pengajuan` (
   `id` int(64) NOT NULL,
   `id_user` int(64) NOT NULL,
-  `pekerjaan` varchar(128) NOT NULL,
-  `penghasilan` int(128) NOT NULL,
-  `pengeluaran` int(128) NOT NULL,
-  `hutang` int(128) NOT NULL,
-  `simpanan` int(128) NOT NULL,
+  `p1` enum('Ya','Tidak') NOT NULL,
+  `p2` enum('Golongan IV','Golongan III','Golongan II','Golongan IB - ID','Golongan IA') NOT NULL,
+  `p3` enum('Ya','Tidak') NOT NULL,
+  `p4` enum('Tidak Ada','Kecil','Sedang','Besar') NOT NULL,
+  `p5` enum('Tidak Ada','<1 Tahun','1 - 5 Tahun','>5 Tahun') NOT NULL,
+  `p6` enum('Di Bawah Rp. 2.500.000','Rp. 2.500.000 - Rp. 5.000.000','Rp. 5.000.000 - Rp. 10.000.000','Di Atas Rp. 10.000.000') NOT NULL,
+  `p7` enum('Di Bawah Rp. 2.500.000','Rp. 2.500.000 - Rp. 5.000.000','Rp. 5.000.000 - Rp. 10.000.000','Di Atas Rp. 10.000.000') NOT NULL,
+  `p8` enum('Tidak Ada','Rp. 2.500.000 - Rp. 5.000.000','Rp. 5.000.000 - Rp. 10.000.000','Di Atas Rp. 10.000.000') NOT NULL,
   `tgl_pengajuan` text NOT NULL,
   `status` varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tab_pengajuan`
 --
 
-INSERT INTO `tab_pengajuan` (`id`, `id_user`, `pekerjaan`, `penghasilan`, `pengeluaran`, `hutang`, `simpanan`, `tgl_pengajuan`, `status`) VALUES
-(2, 6, '4', 4, 5, 5, 1, '0000-00-00', 'Ditolak'),
-(3, 6, '5', 5, 5, 1, 5, '0000-00-00', 'Ditolak'),
-(4, 6, '5', 5, 5, 2, 3, '0000-00-00', 'Ditolak'),
-(5, 6, '5', 4, 3, 3, 3, '0000-00-00', 'Ditolak'),
-(6, 6, '5', 5, 4, 2, 5, '1705415153', 'Ditolak'),
-(7, 6, '5', 4, 4, 3, 4, '16-Jan-2024', 'Diterima'),
-(8, 6, '2', 3, 4, 5, 5, '16 January 2024 21:30', 'Diterima'),
-(9, 6, '4', 5, 4, 3, 4, '16 January 2024 21:33', 'Menunggu');
+INSERT INTO `tab_pengajuan` (`id`, `id_user`, `p1`, `p2`, `p3`, `p4`, `p5`, `p6`, `p7`, `p8`, `tgl_pengajuan`, `status`) VALUES
+(2, 6, '', 'Golongan IB - ID', '', '', 'Tidak Ada', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Menunggu'),
+(3, 6, '', 'Golongan IA', '', 'Tidak Ada', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
+(4, 6, '', 'Golongan IA', '', 'Kecil', '1 - 5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
+(5, 6, '', 'Golongan IB - ID', '', 'Sedang', '1 - 5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
+(6, 6, '', 'Golongan IA', '', 'Kecil', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '1705415153', 'Ditolak'),
+(7, 6, '', 'Golongan IB - ID', '', 'Sedang', '>5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16-Jan-2024', 'Diterima'),
+(8, 6, 'Tidak', 'Golongan II', '', '', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16 January 2024 21:30', 'Diterima'),
+(9, 6, '', 'Golongan IA', '', 'Sedang', '>5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16 January 2024 21:33', 'Menunggu');
 
 -- --------------------------------------------------------
 
@@ -130,24 +134,7 @@ CREATE TABLE `tab_topsis` (
   `id_alternatif` varchar(10) NOT NULL,
   `id_kriteria` varchar(10) NOT NULL,
   `nilai` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `tab_topsis`
---
-
-INSERT INTO `tab_topsis` (`id_alternatif`, `id_kriteria`, `nilai`) VALUES
-('1', '1', 1),
-('1', '2', 5),
-('1', '3', 5),
-('1', '4', 1),
-('1', '5', 4),
-('2', '1', 2),
-('2', '2', 3),
-('2', '3', 3),
-('2', '4', 4),
-('2', '5', 1),
-('Nama Alter', '2', 5);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -165,18 +152,18 @@ CREATE TABLE `users` (
   `dokumen2` varchar(128) DEFAULT NULL,
   `dokumen3` varchar(128) DEFAULT NULL,
   `dokumen4` varchar(128) DEFAULT NULL,
-  `dokumen5` varchar(128) DEFAULT NULL,
   `role` varchar(26) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `dokumen1`, `dokumen2`, `dokumen3`, `dokumen4`, `dokumen5`, `role`) VALUES
-(5, 'admin', 'admin', 'admin@gmail.com', '$2y$10$pIryu1rnBFx2LUSR9wURaeFIqandFeFolpl/Kg11REHFRW.BOFIiG', NULL, NULL, NULL, NULL, NULL, 'admin'),
-(6, 'Teddy Nanta', 'teddy', 'teddy@gmail.com', '$2y$10$m0qm.HCskT9KOj364ruQJ.xyckUEVQdK58XeDt1fNZQYUsbdGV.vW', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', NULL),
-(8, 'Jokowi', 'pimpinan', 'pimpinan@gmail.com', '$2y$10$C066tKn8cuzNSztdmBV48ujgws2iH8rYEQ8E0arggW9wSI4vl7wy2', NULL, NULL, NULL, NULL, NULL, 'pimpinan');
+INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `dokumen1`, `dokumen2`, `dokumen3`, `dokumen4`, `role`) VALUES
+(5, 'admin', 'admin', 'admin@gmail.com', '$2y$10$pIryu1rnBFx2LUSR9wURaeFIqandFeFolpl/Kg11REHFRW.BOFIiG', NULL, NULL, NULL, NULL, 'admin'),
+(6, 'Teddy Nanta', 'teddy', 'teddy@gmail.com', '$2y$10$m0qm.HCskT9KOj364ruQJ.xyckUEVQdK58XeDt1fNZQYUsbdGV.vW', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'user'),
+(8, 'Jokowi', 'pimpinan', 'pimpinan@gmail.com', '$2y$10$C066tKn8cuzNSztdmBV48ujgws2iH8rYEQ8E0arggW9wSI4vl7wy2', NULL, NULL, NULL, NULL, 'pimpinan'),
+(9, 'Bambang Sugiono', 'user', 'user@email.com', '$2y$10$eOYiXa7eob5fJiXj6EbqjOcu0CqxCNXQhQNdW.iS1kOTvfg7kfaIS', 'uploads/gunung.pdf', 'uploads/Dinamika Penduduk Benua Asia.pdf', 'uploads/Alat  Berat dan Pemindahan Tanah Mekanis  - Bab III PENGGUNAAN & KEMAMPUAN ALAT.pdf', 'uploads/DAFTAR_PUSTAKA.pdf', NULL);
 
 --
 -- Indexes for dumped tables
@@ -239,7 +226,7 @@ ALTER TABLE `tab_pengajuan`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables

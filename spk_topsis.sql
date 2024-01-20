c06daf36a4abd7cf85ff7fad1890541c7cb43504
teddynanta <teddy.nanta99@gmail.com>
2024-01-20 10:27:29 +0700

    ubah db

--- spk_topsis.sql
+++ spk_topsis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2024 at 05:17 PM
+-- Generation Time: Jan 20, 2024 at 04:26 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -62,6 +62,7 @@ CREATE TABLE `tab_alternatif` (
 
 INSERT INTO `tab_alternatif` (`id_alternatif`, `nama_alternatif`) VALUES
 ('1', 'jono'),
+('10', 'Dewi Anggun'),
 ('2', 'Wadiyah'),
 ('6', 'Teddy Nanta'),
 ('9', 'Bambang Sugiono');
@@ -84,11 +85,11 @@ CREATE TABLE `tab_kriteria` (
 --
 
 INSERT INTO `tab_kriteria` (`id_kriteria`, `nama_kriteria`, `bobot`, `status`) VALUES
-('c1', 'Karakter', 3, 'Benefit'),
-('c2', 'Kapasitas', 4, 'Benefit'),
-('c3', 'Modal', 5, 'Benefit'),
-('c4', 'Jaminan', 2, 'Benefit'),
-('c5', 'Kondisi', 1, 'Cost');
+('c1', 'Karakter', 0.2, 'Benefit'),
+('c2', 'Kapasitas', 0.1, 'Benefit'),
+('c3', 'Modal', 0.3, 'Cost'),
+('c4', 'Jaminan', 0.2, 'Cost'),
+('c5', 'Kondisi', 0.2, 'Benefit');
 
 -- --------------------------------------------------------
 
@@ -116,15 +117,10 @@ CREATE TABLE `tab_pengajuan` (
 --
 
 INSERT INTO `tab_pengajuan` (`id`, `id_user`, `p1`, `p2`, `p3`, `p4`, `p5`, `p6`, `p7`, `p8`, `tgl_pengajuan`, `status`) VALUES
-(2, 6, '', 'Golongan IB - ID', '', '', 'Tidak Ada', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Menunggu'),
-(3, 6, '', 'Golongan IA', '', 'Tidak Ada', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
-(4, 6, '', 'Golongan IA', '', 'Kecil', '1 - 5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
-(5, 6, '', 'Golongan IB - ID', '', 'Sedang', '1 - 5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '0000-00-00', 'Ditolak'),
-(6, 6, '', 'Golongan IA', '', 'Kecil', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '1705415153', 'Ditolak'),
-(7, 6, '', 'Golongan IB - ID', '', 'Sedang', '>5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16-Jan-2024', 'Diterima'),
 (8, 6, 'Tidak', 'Golongan II', '', '', '', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16 January 2024 21:30', 'Diterima'),
-(9, 6, '', 'Golongan IA', '', 'Sedang', '>5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16 January 2024 21:33', 'Menunggu'),
-(10, 9, 'Ya', 'Golongan III', 'Ya', 'Kecil', '<1 Tahun', 'Rp. 5.000.000 - Rp. 10.000.000', 'Di Atas Rp. 10.000.000', 'Di Atas Rp. 10.000.000', '18 January 2024 22:13', 'Menunggu');
+(9, 6, '', 'Golongan IA', '', 'Sedang', '>5 Tahun', 'Di Bawah Rp. 2.500.000', 'Di Bawah Rp. 2.500.000', 'Tidak Ada', '16 January 2024 21:33', 'Ditolak'),
+(10, 9, 'Ya', 'Golongan III', 'Ya', 'Kecil', '<1 Tahun', 'Rp. 5.000.000 - Rp. 10.000.000', 'Di Atas Rp. 10.000.000', 'Di Atas Rp. 10.000.000', '18 January 2024 22:13', 'Menunggu'),
+(11, 10, 'Tidak', 'Golongan IB - ID', 'Ya', 'Besar', '>5 Tahun', 'Di Atas Rp. 10.000.000', 'Di Atas Rp. 10.000.000', 'Tidak Ada', '19 January 2024 19:01', 'Menunggu');
 
 -- --------------------------------------------------------
 
@@ -133,6 +129,7 @@ INSERT INTO `tab_pengajuan` (`id`, `id_user`, `p1`, `p2`, `p3`, `p4`, `p5`, `p6`
 --
 
 CREATE TABLE `tab_topsis` (
+  `id_pengajuan` varchar(10) NOT NULL,
   `id_alternatif` varchar(10) NOT NULL,
   `id_kriteria` varchar(10) NOT NULL,
   `nilai` float NOT NULL
@@ -142,17 +139,17 @@ CREATE TABLE `tab_topsis` (
 -- Dumping data for table `tab_topsis`
 --
 
-INSERT INTO `tab_topsis` (`id_alternatif`, `id_kriteria`, `nilai`) VALUES
-('6', 'c1', 50),
-('6', 'c2', 70),
-('6', 'c3', 60),
-('6', 'c4', 60),
-('6', 'c5', 50),
-('9', 'c1', 90),
-('9', 'c2', 80),
-('9', 'c3', 60),
-('9', 'c4', 50),
-('9', 'c5', 50);
+INSERT INTO `tab_topsis` (`id_pengajuan`, `id_alternatif`, `id_kriteria`, `nilai`) VALUES
+('11', '10', 'c1', 90),
+('11', '10', 'c2', 80),
+('11', '10', 'c3', 90),
+('11', '10', 'c4', 50),
+('11', '10', 'c5', 80),
+('9', '6', 'c1', 80),
+('9', '6', 'c2', 90),
+('9', '6', 'c3', 70),
+('9', '6', 'c4', 60),
+('9', '6', 'c5', 50);
 
 -- --------------------------------------------------------
 
@@ -181,7 +178,8 @@ INSERT INTO `users` (`id`, `nama`, `username`, `email`, `password`, `dokumen1`, 
 (5, 'admin', 'admin', 'admin@gmail.com', '$2y$10$pIryu1rnBFx2LUSR9wURaeFIqandFeFolpl/Kg11REHFRW.BOFIiG', NULL, NULL, NULL, NULL, 'admin'),
 (6, 'Teddy Nanta', 'teddy', 'teddy@gmail.com', '$2y$10$m0qm.HCskT9KOj364ruQJ.xyckUEVQdK58XeDt1fNZQYUsbdGV.vW', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'uploads/none.pdf', 'user'),
 (8, 'Jokowi', 'pimpinan', 'pimpinan@gmail.com', '$2y$10$C066tKn8cuzNSztdmBV48ujgws2iH8rYEQ8E0arggW9wSI4vl7wy2', NULL, NULL, NULL, NULL, 'pimpinan'),
-(9, 'Bambang Sugiono', 'user', 'user@email.com', '$2y$10$eOYiXa7eob5fJiXj6EbqjOcu0CqxCNXQhQNdW.iS1kOTvfg7kfaIS', 'uploads/gunung.pdf', 'uploads/Dinamika Penduduk Benua Asia.pdf', 'uploads/Alat  Berat dan Pemindahan Tanah Mekanis  - Bab III PENGGUNAAN & KEMAMPUAN ALAT.pdf', 'uploads/DAFTAR_PUSTAKA.pdf', NULL);
+(9, 'Bambang Sugiono', 'user', 'user@email.com', '$2y$10$eOYiXa7eob5fJiXj6EbqjOcu0CqxCNXQhQNdW.iS1kOTvfg7kfaIS', 'uploads/gunung.pdf', 'uploads/Dinamika Penduduk Benua Asia.pdf', 'uploads/Alat  Berat dan Pemindahan Tanah Mekanis  - Bab III PENGGUNAAN & KEMAMPUAN ALAT.pdf', 'uploads/DAFTAR_PUSTAKA.pdf', NULL),
+(10, 'Dewi Anggun', 'dewi', 'dewi@gmail.com', '$2y$10$WxI26Kufy9/9Vphg3Z2rsu49NDjVJ2jQz7yz7HPd6iHHQr7Y6/Xv2', 'uploads/progit.pdf', 'uploads/701-1433-1-PB.pdf', 'uploads/2969-6793-2-PB.pdf', 'uploads/1103-2185-1-SM.pdf', NULL);
 
 --
 -- Indexes for dumped tables
@@ -238,13 +236,13 @@ ALTER TABLE `data`
 -- AUTO_INCREMENT for table `tab_pengajuan`
 --
 ALTER TABLE `tab_pengajuan`
-  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables

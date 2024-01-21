2cea1bdbde1165ee63df5616ed6f10a20b6f8066
teddynanta <teddy.nanta99@gmail.com>
2024-01-21 10:38:46 +0700

    hapus file tidak penting

--- pengajuan.php
+++ pengajuan.php
@@ -3,6 +3,7 @@
 session_start();
 include "koneksi.php";
 include "head.php";
+$uid = $_SESSION['uid'];
 ?>
 
 <body class="d-flex flex-column h-100">
@@ -19,86 +20,74 @@ include "head.php";
         <h6 class="m-0 font-weight-bold text-primary text-center">Data Pemohon</h6>
       </div>
       <div class="card-body">
+        <p>Isikan formulir ini dengan sebenar-benarnya.</p>
+        <p>Setelah selesai anda akan diberikan formulir cetak berbentuk PDF.</p>
+        <br>
+        <form action="pd_pemohon.php" method="post">
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">ID User</label>
+            <input type="text" id="disabledTextInput" name="iduser" class="form-control" value="<?php echo $_SESSION['uid'] ?>" readonly>
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Nama Sesuai KTP</label>
+            <input type="text" id="disabledTextInput" name="nama" class="form-control" value="<?php echo $_SESSION['nama'] ?>">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Tanggal Lahir</label>
+            <input type="date" id="disabledTextInput" name="bdate" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Kewarganegaraan</label>
+            <select class="form-select" aria-label="Default select example" name="warga">
+              <option selected>Jawaban</option>
+              <option value="1">Warga Negara Indonesia</option>
+              <option value="2">Warga Negara Asing (WNA)</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">No. KTP</label>
+            <input type="number" id="disabledTextInput" name="nik" class="form-control" placeholder="Masukan 16 Digit No. KTP">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">No. NPWP (untuk pinjaman di atas 100JT) &nbsp; <span class="text-danger">*kosongkan apabila tidak perlu</span></label>
+            <input type="text" id="disabledTextInput" name="npwp" class="form-control" placeholder="Masukan No. NPWP">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Nama Gadis Ibu Kandung</label>
+            <input type="text" id="disabledTextInput" name="ibu" class="form-control" placeholder="Masukan Nama Gadis Ibu Kandung Anda">
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Status Pernikahan</label>
+            <select class="form-select" aria-label="Default select example" name="nikah">
+              <option selected>Jawaban</option>
+              <option value="1">Belum Menikah</option>
+              <option value="2">Menikah</option>
+              <option value="3">Janda/Duda</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Nama Istri/Suami &nbsp; <span class="text-danger">*kosongkan apabila tidak ada</span></label></label>
+            <input type="text" id="disabledTextInput" name="pnama" class="form-control" placeholder="Masukan Nama Pasangan Anda">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Tanggal Lahir Istri/Suami &nbsp; <span class="text-danger">*kosongkan apabila tidak ada</span></label></label></label>
+            <input type="date" id="disabledTextInput" name="pbdate" class="form-control">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Alamat Sesuai KTP</label>
+            <input type="text" id="disabledTextInput" name="alamat" class="form-control" placeholder="Masukan Alamat Anda">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">Alamat Sesuai Domisili</label>
+            <input type="text" id="disabledTextInput" name="alamatdom" class="form-control" placeholder="Masukan Alamat Anda">
+          </div>
+          <div class="mb-3">
+            <label for="disabledTextInput" class="form-label">No. Telepon yang Dapat Dihubungi</label>
+            <input type="text" id="disabledTextInput" name="notel" class="form-control" placeholder="Masukan Alamat Anda">
+          </div>
 
-        <?php
-        $uid = $_SESSION['uid'];
-        // $users = $koneksi->query("SELECT dokumen1, dokumen2, dokumen3, dokumen4, dokumen5 FROM users WHERE id ='$uid'");
-        $users = $koneksi->query("SELECT dokumen1, dokumen2, dokumen3, dokumen4 FROM users WHERE id ='$uid' AND dokumen1 IS NOT NULL AND dokumen2 IS NOT NULL AND dokumen3 IS NOT NULL AND dokumen4 IS NOT NULL");
-        if ($users->fetch_assoc() == null) : ?>
-          <h5>Maaf, anda belum bisa melakukan pengajuan.</h5>
-          <p>Dokumen pelengkap anda belum lengkap, lengkapi terlebih dahulu dokumen anda untuk dapat melakukan pengajuan pinjaman!</p>
-          <a class="btn btn-primary" href="profil.php">Edit Profil</a>
-        <?php else : ?>
-          <p>Isikan formulir ini dengan sebenar-benarnya.</p>
-          <p>Setelah selesai anda akan diberikan formulir cetak berbentuk PDF.</p>
-          <br>
-          <form action="tambahpengajuan.php" method="post">
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">ID User</label>
-              <input type="text" id="disabledTextInput" name="iduser" class="form-control" value="<?php echo $_SESSION['uid'] ?>" readonly>
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Nama Sesuai KTP</label>
-              <input type="text" id="disabledTextInput" name="nama" class="form-control" value="<?php echo $_SESSION['nama'] ?>">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Tanggal Lahir</label>
-              <input type="date" id="disabledTextInput" name="bdate" class="form-control">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Kewarganegaraan</label>
-              <select class="form-select" aria-label="Default select example" name="warga">
-                <option selected>Jawaban</option>
-                <option value="1">Warga Negara Indonesia</option>
-                <option value="2">Warga Negara Asing (WNA)</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">No. KTP</label>
-              <input type="number" id="disabledTextInput" name="nik" class="form-control" placeholder="Masukan 16 Digit No. KTP">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">No. NPWP (untuk pinjaman di atas 100JT) &nbsp; <span class="text-danger">*kosongkan apabila tidak perlu</span></label>
-              <input type="number" id="disabledTextInput" name="npwp" class="form-control" placeholder="Masukan No. NPWP">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Nama Gadis Ibu Kandung</label>
-              <input type="text" id="disabledTextInput" name="ibu" class="form-control" placeholder="Masukan Nama Gadis Ibu Kandung Anda">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Status Pernikahan</label>
-              <select class="form-select" aria-label="Default select example" name="statusnikah">
-                <option selected>Jawaban</option>
-                <option value="1">Belum Menikah</option>
-                <option value="2">Menikah</option>
-                <option value="3">Janda/Duda</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Nama Istri/Suami &nbsp; <span class="text-danger">*kosongkan apabila tidak ada</span></label></label>
-              <input type="text" id="disabledTextInput" name="ibu" class="form-control" placeholder="Masukan Nama Pasangan Anda">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Tanggal Lahir Istri/Suami &nbsp; <span class="text-danger">*kosongkan apabila tidak ada</span></label></label></label>
-              <input type="date" id="disabledTextInput" name="pbdate" class="form-control">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Alamat Sesuai KTP</label>
-              <input type="text" id="disabledTextInput" name="alamat" class="form-control" placeholder="Masukan Alamat Anda">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">Alamat Sesuai Domisili</label>
-              <input type="text" id="disabledTextInput" name="alamatdom" class="form-control" placeholder="Masukan Alamat Anda">
-            </div>
-            <div class="mb-3">
-              <label for="disabledTextInput" class="form-label">No. Telepon yang Dapat Dihubungi</label>
-              <input type="text" id="disabledTextInput" name="alamatdom" class="form-control" placeholder="Masukan Alamat Anda">
-            </div>
-
-            <button type="submit" class="btn btn-primary">Buat Pengajuan</button>
-          </form>
-        <?php endif;
-        ?>
+          <button type="submit" class="btn btn-primary">Buat Pengajuan</button>
+        </form>
       </div>
     </div>
   </div>
